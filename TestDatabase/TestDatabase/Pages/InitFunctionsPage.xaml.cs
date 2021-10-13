b20006a6440de9d55979a2a3aac3ce28caf66ddf
Ale Escobar <a28esc@gmail.com>
2021-10-13 15:46:02 -0400

    Added InitFunctions Page
    Added InitFunctions Page xaml and xaml.cs

---
+++ TestDatabase/TestDatabase/Pages/InitFunctionsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TestDatabase.Pages
+{
+    public partial class InitFunctionsPage : ContentPage
+    {
+        public InitFunctionsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
